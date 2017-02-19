a3de9cf41f4ec34f4ead91a6212e2f0c6f7189b7
y4shiro <y4shironao@gmail.com>
2017-02-20 00:51:13 +0900

    P85

--- app/controllers/helo_controller.rb
+++ app/controllers/helo_controller.rb
@@ -1,18 +1,4 @@
 class HeloController < ApplicationController
   def index
-    if params['msg'] != nil then
-      msg = 'Hello, ' + params['msg'] + '!'
-    else
-      msg = 'this is sample page.'
-    end
-    html = '
-    <html>
-    <body>
-      <h1>Sample Page</h1>
-      <p>' + msg + '</p>
-    </body>
-    </html>
-    '
-    render html: html.html_safe
   end
 end
