a7c943674727c6d870b654ee1395cf130c3915ec
y4shiro <y4shironao@gmail.com>
2017-03-02 00:36:53 +0900

    P133 Cleate message borad

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  get 'dengonban', to: 'dengonban#index'
+  post 'dengonban', to: 'dengonban#index'
   get 'dengonban/index'
+  post 'dengonban/index'
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'helo/index'
   get 'helo/other'
   get 'helo', to: 'helo#index'
