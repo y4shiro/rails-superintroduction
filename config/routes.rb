645c32766eb22195790e5801bc6b20151b4cd069
y4shiro <y4shironao@gmail.com>
2017-03-03 00:33:31 +0900

    P173

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,16 @@
 Rails.application.routes.draw do
   get 'people/index'
+  get 'people', to: 'people#index'
 
-  get 'dengonban', to: 'dengonban#index'
-  post 'dengonban', to: 'dengonban#index'
   get 'dengonban/index'
   post 'dengonban/index'
+  get 'dengonban', to: 'dengonban#index'
+  post 'dengonban', to: 'dengonban#index'
 
   get 'helo/index'
   get 'helo/other'
   get 'helo', to: 'helo#index'
 
-  post 'helo', to: 'helo#index'
   post 'helo/index'
+  post 'helo', to: 'helo#index'
 end
