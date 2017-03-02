21da069dab199b6816a3a6610c83700e0d409ddc
y4shiro <y4shironao@gmail.com>
2017-03-03 00:43:52 +0900

    P177

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -3,4 +3,9 @@ class PeopleController < ApplicationController
     @msg = 'Person data.'
     @data = Person.all
   end
+
+  def show
+    @msg = "Indexed data."
+    @data = Person.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get 'people/index'
   get 'people', to: 'people#index'
+  get 'people/:id', to: 'people#show'
 
   get 'dengonban/index'
   post 'dengonban/index'
