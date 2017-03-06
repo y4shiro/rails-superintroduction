4e17285e5b33fc89aa69ba93b4fc06dc8a61579c
y4shiro <y4shironao@gmail.com>
2017-03-07 01:16:21 +0900

    P197

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -22,6 +22,17 @@ class PeopleController < ApplicationController
     redirect_to '/people'
   end
 
+  def edit
+    @msg = "edit data.[id = " + params[:id] + "]"
+    @person = Person.find(params[:id])
+  end
+
+  def update
+    obj = Person.find(params[:id])
+    obj.update(person_params)
+    redirect_to '/people'
+  end
+
   private
   def person_params
     params.require(:person).permit(:name, :age, :mail)
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
   get 'people/index'
   get 'people', to: 'people#index'
   get 'people/:id', to: 'people#show'
+  get 'people/edit/:id', to: 'people#edit'
+  post 'people/edit/:id', to: 'people#update'
+  patch 'people/edit/:id', to: 'people#update'
 
   get 'dengonban/index'
   post 'dengonban/index'
