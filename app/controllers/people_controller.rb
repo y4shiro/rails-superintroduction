b85dc0ceb40a166ef68ccf2a50ee6b13e4a7cf7a
y4shiro <y4shironao@gmail.com>
2017-07-13 00:58:50 +0900

    P233 like

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -44,7 +44,8 @@ class PeopleController < ApplicationController
     @msg = 'please type search word...'
     @people = Array.new
     if request.post? then
-      @people = Person.where "age >= ?", params['find']
+      @people = Person.where "mail like ?",
+        '%' + params['find'] + '%'
     end
   end
 
