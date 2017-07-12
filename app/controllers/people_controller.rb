a76c8a914aab3d98ae385197a45ea7d8e859e1a7
y4shiro <y4shironao@gmail.com>
2017-07-13 01:14:04 +0900

    P240

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -44,10 +44,8 @@ class PeopleController < ApplicationController
     @msg = 'please type search word...'
     @people = Array.new
     if request.post? then
-      f = '%' + params[:find] + '%'
-      result = Person.where "name like ? or mail like ?", f, f
-      @people.push result.first
-      @people.push result.last
+      f = params[:find].split(',')
+      @people = Person.find(f)
     end
   end
 
