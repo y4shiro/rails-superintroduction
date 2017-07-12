7993db2f23354c4b8cd6aad2733dd4b0231d5013
y4shiro <y4shironao@gmail.com>
2017-07-13 01:07:54 +0900

    P236

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -44,8 +44,8 @@ class PeopleController < ApplicationController
     @msg = 'please type search word...'
     @people = Array.new
     if request.post? then
-      f = params[:find].split ','
-      @people = Person.where "age >= ? and age <= ?", f[0], f[1]
+      f = '%' + params[:find] + '%'
+      @people = Person.where "name like ? or mail like ?", f, f
     end
   end
 
