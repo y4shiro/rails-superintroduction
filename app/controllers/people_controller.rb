0a308750151f174cdfd43910b4f5ed6adb377c62
y4shiro <y4shironao@gmail.com>
2017-07-14 00:38:44 +0900

    P248

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -17,10 +17,13 @@ class PeopleController < ApplicationController
   end
 
   def create
-    if request.post? then
-      Person.create(person_params)
+    @person = Person.new person_params
+    if @person.save then
+      redirect_to '/people'
+    else
+      @msg = '入力に問題があります'
+      render 'add'
     end
-    redirect_to '/people'
   end
 
   def edit
