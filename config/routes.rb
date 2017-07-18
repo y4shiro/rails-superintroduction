518308cc6694c646fae42badac537bed9dba1aab
y4shiro <y4shironao@gmail.com>
2017-07-19 00:09:20 +0900

    P275 messages index

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
   get 'messages/index'
-
-  get 'messages/show'
+  get 'messages', to: 'messages#index'
 
   get 'messages/add'
+  post 'messages/add', to: 'messages#create'
 
-  get 'messages/edit'
+  get 'messages/edit/:id', to: 'messages#edit'
+  post 'messages/edit/:id', to: 'messages#update'
+
+  get 'messages/delete/:id' to: 'messages#delete'
+  get 'messages/:id', to: 'messages#show'
 
   get 'cards/index'
   get 'cards', to: 'cards#index'
