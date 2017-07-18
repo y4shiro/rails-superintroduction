375d4af53ccfd7d2312d841a1598d5e6734da794
y4shiro <y4shironao@gmail.com>
2017-07-18 23:43:08 +0900

    P270 generate messages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'messages/index'
+
+  get 'messages/show'
+
+  get 'messages/add'
+
+  get 'messages/edit'
+
   get 'cards/index'
   get 'cards', to: 'cards#index'
   get 'cards/show'
