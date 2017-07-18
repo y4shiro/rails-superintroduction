2cf0630733da1547d789b31c7add0f9d3e0661b3
y4shiro <y4shironao@gmail.com>
2017-07-19 01:13:05 +0900

    P271 fixed controlelr

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -44,6 +44,6 @@ class MessagesController < ApplicationController
 
   private
   def message_params
-    params.require(:message).permit(:permit_id, :title, :message)
+    params.require(:message).permit(:person_id, :title, :message)
   end
 end
