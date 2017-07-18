29a0405ffbed5b0f2df70a149c6d0037e446231a
y4shiro <y4shironao@gmail.com>
2017-07-18 23:41:28 +0900

    P269 migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170307152519) do
+ActiveRecord::Schema.define(version: 20170718144024) do
 
   create_table "cards", force: :cascade do |t|
     t.text     "title"
@@ -18,6 +18,14 @@ ActiveRecord::Schema.define(version: 20170307152519) do
     t.integer  "price"
     t.text     "publisher"
     t.text     "memo"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.integer  "person_id"
+    t.text     "title"
+    t.text     "message"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
