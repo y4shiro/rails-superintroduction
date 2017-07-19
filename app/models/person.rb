9ddfb81ed9d51461ec7e7c5e49194cf828a4bd22
y4shiro <y4shironao@gmail.com>
2017-07-20 00:16:31 +0900

    P281

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,4 +1,6 @@
 class Person < ApplicationRecord
+  has_one :message
+
   validates :name, presence: { message: 'は、必須項目です。' }
   validates :age, numericality: { message: 'は、数字で入力してください。' }
   validates :mail, email: { message: 'はメールアドレスではありません。' }
