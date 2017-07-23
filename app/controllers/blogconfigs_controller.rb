aa984aff7bee612fb924d61094b3cab1b1c24349
y4shiro <y4shironao@gmail.com>
2017-07-23 23:51:50 +0900

    P298 create blogconfigs controller

--- app/controllers/blogconfigs_controller.rb
+++ app/controllers/blogconfigs_controller.rb
@@ -1,7 +1,20 @@
 class BlogconfigsController < ApplicationController
+  layout 'blogconfigs'
+
   def index
+    @blogconfig = Blogconfig.find 1
   end
 
   def edit
+    @blogconfig = Blogconfig.find 1
+    if request.patch? then
+      @blogconfig.update blogconfig_params
+      redirect_to '/blogcongigs'
+    end
+  end
+
+  private
+  def blogconfigs_params
+    params.require(:blogconfig).permit(:title, :subtitle, :stylename)
   end
 end
